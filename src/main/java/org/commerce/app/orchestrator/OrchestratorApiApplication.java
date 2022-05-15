019658e519bef35bf4848f0c6aea6f6878b80afb
Anadi Nema <nema.anadi99@gmail.com>
2022-05-15 13:41:49 +0530

    Initial project commit

---
+++ src/main/java/org/commerce/app/orchestrator/OrchestratorApiApplication.java
@@ -0,0 +1,13 @@
+package org.commerce.app.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/commerce/app/orchestrator/OrchestratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commerce.app.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
